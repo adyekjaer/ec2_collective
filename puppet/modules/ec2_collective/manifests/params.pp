5746b8be7a2b6cd13cd29b3c5c5e8155a6186134
Anders Dyekjær Hansen <adh@dyekjaer.dk>
2014-05-05 11:40:22 +0200

    new release

--- puppet/modules/ec2_collective/manifests/params.pp
+++ puppet/modules/ec2_collective/manifests/params.pp
@@ -1,6 +1,6 @@
 class ec2_collective::params {
     $run_as                     = 'root'
-    $release                    = '2014050501'
+    $release                    = '2014050502'
     $install_deps               = true
     $boto_version               = '2.27.0'
     $deps_from_pip              = false
