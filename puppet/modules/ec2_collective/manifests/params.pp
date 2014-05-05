204311d03e504c503181991b73860bde4086cd08
Anders Dyekjær Hansen <adh@dyekjaer.dk>
2014-05-05 16:06:44 +0200

    bump release

--- puppet/modules/ec2_collective/manifests/params.pp
+++ puppet/modules/ec2_collective/manifests/params.pp
@@ -1,6 +1,6 @@
 class ec2_collective::params {
     $run_as                     = 'root'
-    $release                    = '2014050502'
+    $release                    = '2014050503'
     $install_deps               = true
     $boto_version               = '2.27.0'
     $deps_from_pip              = false
